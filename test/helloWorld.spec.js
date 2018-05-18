c52d15a7fdacfed69d953efdd1d2875473f9d502
Krzysztof <kjendrzyca@gmail.com>
2018-05-18 09:57:49 +0200

    Add airbnb eslint config and fix error

--- test/helloWorld.spec.js
+++ test/helloWorld.spec.js
@@ -2,36 +2,36 @@ import assert from 'assert';
 import HelloWorld from '../src/helloWorld';
 
 describe('HelloWorld', () => {
-	it(`should return 'Hello' for sayHello function call`, () => {
-		// given
-    const sayHello = HelloWorld.sayHello;
+  it('should return \'Hello\' for sayHello function call', () => {
+    // given
+    const { sayHello } = HelloWorld;
 
-		// when
-		const hello = sayHello();
+    // when
+    const hello = sayHello();
 
-		// then
-		assert.equal(hello, 'Hello');
-	});
+    // then
+    assert.equal(hello, 'Hello');
+  });
 
-	it(`should return 'World' for sayWorld function call`, () => {
-		// given
-		const sayWorld = HelloWorld.sayWorld;
+  it('should return \'World\' for sayWorld function call', () => {
+    // given
+    const { sayWorld } = HelloWorld;
 
-		// when
-		const world = sayWorld();
+    // when
+    const world = sayWorld();
 
-		// then
-		assert.equal(world, 'World');
-	});
+    // then
+    assert.equal(world, 'World');
+  });
 
-	it(`should return 'Hello World' for sayHelloWorld function call`, () => {
-		// given
-		const sayHelloWorld = HelloWorld.sayHelloWorld;
+  it('should return \'Hello World\' for sayHelloWorld function call', () => {
+    // given
+    const { sayHelloWorld } = HelloWorld;
 
-		// when
-		const helloWorld = sayHelloWorld();
+    // when
+    const helloWorld = sayHelloWorld();
 
-		// then
-		assert(helloWorld, 'Hello World');
-	});
+    // then
+    assert(helloWorld, 'Hello World');
+  });
 });
