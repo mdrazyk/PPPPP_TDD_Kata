6520059186967306c1a5121b08976940482d9b54
roberthebel <robert@brainhub.pl>
2018-05-22 18:35:22 +0200

    negative values checker

--- src/calculator.js
+++ src/calculator.js
@@ -3,6 +3,12 @@ class calculator {
     let result = 0;
     const numbers = inputString.match(/\d+/g);
 
+    const negativeNumbers = inputString.match(/\-./g);
+
+    if (negativeNumbers) {
+      throw new Error(`Negatives not allowed. '${negativeNumbers}'`)
+    }
+
     if (numbers) {
       return numbers.reduce((curr, prev) => Number(prev) + Number(curr), 0);
     }
