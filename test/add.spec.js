8f711df2bb4c2ec431428fbaa08166145d9cc7ff
Piotr Sroczkowski <piotr.sroczkowski@brainhub.pl>
2018-05-22 17:51:07 +0200

    test return input

--- test/add.spec.js
+++ test/add.spec.js
@@ -12,5 +12,17 @@ describe('Add function', () => {
 
     // then
     assert(result === expected);
-  })
+  });
+
+  it('should return input if there is only one arguments and it is not 0', () => {
+    // given
+    const input = 5;
+    const expected = 5;
+
+    //when
+    const result = add(input);
+
+    // then
+    assert(result === expected);
+  });
 });
