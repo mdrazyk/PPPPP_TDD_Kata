ddd8c20dcccf9ccbbee8305488a1253c17c50f29
Bartlomiej <bartek@brainhub.pl>
2018-05-22 18:35:13 +0200

    end of part 2

--- src/calc.js
+++ src/calc.js
@@ -1,8 +1,30 @@
 export default function add(value) {
-  return value
-    .split(',')
-    .reduce(
-      (acc, currentValue) => acc + (parseInt(currentValue, 10) || 0),
-      0,
-    );
+
+  if( /-\d/.test(value) ) {
+    throw new Error('negatives not allowed');
+  } else {
+
+    const delimiters = value
+      .match(/\[.*\]/g)
+
+    if (delimiters) {
+      const delimiterWithout = delimiters.map(x => {
+        return x.replace('[','').replace(']','');
+      });
+    }
+
+    return value
+      .split(/\D/g)
+      .reduce(
+        (acc, currentValue) => {
+          const value = parseInt(currentValue, 10) || 0;
+          if(value>=1000){
+            return acc;
+          }
+
+          return acc+value;
+        }
+        ,0
+      );
+  }
 }
--- test/calc.spec.js
+++ test/calc.spec.js
@@ -46,15 +46,69 @@ describe('Calc', () => {
 		assert.equal(returnedValue, 11)
 	});
 
-
-	it(`alllows using \n instead of commas`, () => {
+	it(`alllows using \\n instead of commas`, () => {
 		// given
-		const value = '1\n2,3,5';
+		const value = '1\n2,3';
 
 		// when
 		const returnedValue = add(value);
 
 		//then
-		assert.equal(returnedValue, 11)
+		assert.equal(returnedValue, 6)
+	});
+
+	it(` “//;\n1;2” should return 3`, () => {
+		// given
+		const value = '//;\n1;2';
+
+		// when
+		const returnedValue = add(value);
+
+		//then
+		assert.equal(returnedValue, 3)
+	});
+
+	it(` negative numbert should throw exception`, () => {
+		// given
+		const value = '1;-2';
+
+		// when
+		//const returnedValue = add(value);
+
+		//then
+		assert.throws( ()=>add(value), 'negatives not allowed' );
+	});
+
+	it(`numbers bigger than 1000 are ignored`, () => {
+		// given
+		const value = '1,2,1000,2';
+
+		// when
+		const returnedValue = add(value);
+
+		//then
+		assert.equal(returnedValue, 5);
+	});
+
+	it(`“//[***]\n1***2***3”  shoutd return 6`, () => {
+		// given
+		const value = "//[***]\n1***2***3";
+
+		// when
+		const returnedValue = add(value);
+
+		//then
+		assert.equal(returnedValue, 6);
+	});
+
+	it(`“//[***][xxx]\n1***2*??*3@#$@#$2”  shoutd return 6`, () => {
+		// given
+		const value = "//[***][xxx]\n1***2***3";
+
+		// when
+		const returnedValue = add(value);
+
+		//then
+		assert.equal(returnedValue, 6);
 	});
 });
