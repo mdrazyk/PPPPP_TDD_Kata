76049408364dfee67813da3bbfb7d8c79d2381cb
Mateusz Drażyk <=mateusz.drazyk@brainhub.pl>
2018-05-22 18:30:39 +0200

    should return exception for input '-1,-1'

--- src/calculator.js
+++ src/calculator.js
@@ -1,14 +1,13 @@
 class calculator {
-   static Add(inputString){
-     let result = 0;
-
-    if(inputString.length===0){
-      return 0;
-    }
-
+  static Add(inputString) {
+    let result = 0;
     const numbers = inputString.match(/\d+/g);
 
-    return numbers.reduce((curr, prev) => Number(prev) + Number(curr), 0);
+    if (numbers) {
+      return numbers.reduce((curr, prev) => Number(prev) + Number(curr), 0);
+    }
+
+    return 0;
   }
 }
 
--- test/calculator.spec.js
+++ test/calculator.spec.js
@@ -13,9 +13,10 @@ describe('Calculator', () => {
     assert.equal(result, 0);
   });
 
-  it('should return 3 for single number', () => {
+  it(`should return 3 for input '3'`, () => {
     // given
     const numberString = '3';
+    const expected = 3;
 
     // when
     const result = Calculator.Add(numberString);
@@ -25,28 +26,30 @@ describe('Calculator', () => {
 
   })
 
-  it(`should return 5 for sum of 2 numbers in string`, () => {
+  it(`should return 5 for input '3,2'`, () => {
     // given
     const numbersString = '3,2';
+    const expected = 5
 
     // when
     const result = Calculator.Add(numbersString);
 
     // then
-    assert.equal(result, 5);
+    assert.equal(result, expected);
   });
 
-  it(`should return 22 for sum of numbers in string`, () => {
+  it(`should return 22 for input '2,2,2,2,2,2,2,2,2,2,2'`, () => {
     // given
     const numbersString = '2,2,2,2,2,2,2,2,2,2,2';
+    const expected = 22;
 
     // when
     const result = Calculator.Add(numbersString);
 
     // then
-    assert.equal(result, 22);
+    assert.equal(result, expected);
   });
-  it(`should return 2 for sum of 2 in string`, () => {
+  it(`should return 2 for input '2\\n2'`, () => {
     // given
     const numbersString = '2\n2';
 
@@ -56,14 +59,28 @@ describe('Calculator', () => {
     // then
     assert.equal(result, 4);
   });
-  it(`should return 0 when new line is at the of the string`, () => {
+
+  it(`should return 3 for input '//;\n1;2'`, () => {
     // given
-    const numbersString = '1,\\n';
+    const numbersString = '//;\n1;2';
+    const expected = 3;
 
     // when
     const result = Calculator.Add(numbersString);
 
     // then
-    assert.equal(result, 0);
+    assert.equal(result, expected);
+  });
+
+  it(`should return exception for input '-1,-1'`, () => {
+    // given
+    const numbersString = '-1,-1';
+    const expected = 'Negatives not allowed. -1,-1';
+
+    // when
+    const result = Calculator.Add(numbersString);
+
+    // then
+    assert.equal(result, expected);
   });
 });
