03ecb206467d93702b82c199b328b10529dec425
Piotr Sroczkowski <piotr.sroczkowski@brainhub.pl>
2018-05-22 18:10:26 +0200

    fix add many

--- src/add.js
+++ src/add.js
@@ -1,9 +1,5 @@
 export default function add(input) {
-  const [aAsString = 0, bAsString = 0] = input.split(',');
-  const a = Number(aAsString);
-  const b = Number(bAsString);
+  const result = input.split(',').map(x => Number(x)).reduce((prev, curr) => prev + curr , 0);
 
-  const result = input.split(',').reduce((prev, curr) => prev + curr ,0);
-
-  return a + b;
+  return result;
 }
